8492ee2b5d42263fac9bb0f64276a7ecbf5a70bf
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-03-20 14:14:26 -0700

    Create skeleton module and factory fcn

---
+++ script.js
@@ -0,0 +1,14 @@
+// Global module
+const gameModule = (() => {
+  const gameBoard = [];
+})();
+
+// Players
+const playerFactory = (name, marker) => {
+  // code goes here
+};
+
+// FUNCTION: display game board
+function displayBoard() {
+  // code goes here
+}
