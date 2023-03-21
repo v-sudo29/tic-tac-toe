df1ebda3bb68d8bd5ba03eea4c212b90a43b7026
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-03-21 16:02:55 -0700

    Complete logic for tic-tac-toe win

--- script.js
+++ script.js
@@ -8,6 +8,61 @@ const gameModule = (() => {
   // Array holds markers
   const gameBoard = ['', '', '', '', '', '', '', '', ''];
 
+  // FUNCTION: check if a player has won
+  const checkWinner = () => {
+    const winner = null;
+    const markerOne = document.querySelector('.marker-one').innerHTML;
+    const markerTwo = document.querySelector('.marker-two').innerHTML;
+    const announcer = document.querySelector('.announcer');
+
+    // Horizontal match
+    if ((gameBoard[0] === markerOne) && (gameBoard[1] === markerOne) && (gameBoard[2] === markerOne)) {
+      console.log('Player One wins!');
+    } else if ((gameBoard[3] === markerOne) && (gameBoard[4] === markerOne) && (gameBoard[5] === markerOne)) {
+      console.log('Player One wins!');
+    } else if (gameBoard[6] === markerOne && gameBoard[7] === markerOne && gameBoard[8] === markerOne) {
+      console.log('Player One wins!');
+    }
+
+    if ((gameBoard[0] === markerTwo) && (gameBoard[1] === markerTwo) && (gameBoard[2] === markerTwo)) {
+      console.log('Player Two wins!');
+    } else if ((gameBoard[3] === markerTwo) && (gameBoard[4] === markerTwo) && (gameBoard[5] === markerTwo)) {
+      console.log('Player Two wins!');
+    } else if (gameBoard[6] === markerTwo && gameBoard[7] === markerTwo && gameBoard[8] === markerTwo) {
+      console.log('Player Two wins!');
+    }
+
+    // Vertical match
+    if (gameBoard[0] === markerOne && gameBoard[3] === markerOne && gameBoard[6] === markerOne) {
+      console.log('Player One wins!');
+    } else if (gameBoard[1] === markerOne && gameBoard[4] === markerOne && gameBoard[7] === markerOne) {
+      console.log('Player One wins!');
+    } else if (gameBoard[2] === markerOne && gameBoard[5] === markerOne && gameBoard[8] === markerOne) {
+      console.log('Player One wins!');
+    }
+
+    if ((gameBoard[0] === markerTwo) && (gameBoard[3] === markerTwo) && (gameBoard[6] === markerTwo)) {
+      console.log('Player Two wins!');
+    } else if ((gameBoard[1] === markerTwo) && (gameBoard[4] === markerTwo) && (gameBoard[7] === markerTwo)) {
+      console.log('Player Two wins!');
+    } else if ((gameBoard[2] === markerTwo) && (gameBoard[5] === markerTwo) && (gameBoard[8] === markerTwo)) {
+      console.log('Player Two wins!');
+    }
+
+    // Diagonal match
+    if (gameBoard[0] === markerOne && gameBoard[4] === markerOne && gameBoard[8] === markerOne) {
+      console.log('Player One wins!');
+    } else if (gameBoard[2] === markerOne && gameBoard[4] === markerOne && gameBoard[6] === markerOne) {
+      console.log('Player One wins!');
+    }
+
+    if ((gameBoard[0] === markerTwo) && (gameBoard[4] === markerTwo) && (gameBoard[8] === markerTwo)) {
+      console.log('Player Two wins!');
+    } else if (gameBoard[2] === markerTwo && gameBoard[4] === markerTwo && gameBoard[6] === markerTwo) {
+      console.log('Player Two wins!');
+    }
+  }
+
   // Display game board tiles
   const displayBoard = () => {
     const boardContainer = document.querySelector('.gameboard-container');
@@ -35,7 +90,7 @@ const gameModule = (() => {
 
   // Populate empty tile with current player's marker
   const populate = (gameTile) => {
-    
+
     // Add hover
     gameTile.classList.add('empty');
 
@@ -45,19 +100,39 @@ const gameModule = (() => {
         const markerTwo = document.querySelector('.marker-two').innerHTML;
         const middleFooterContent = document.querySelector('.middle-footer');
         const childDiv = middleFooterContent.firstChild;
+        const index = gameTile.id;
 
         if (childDiv.classList.contains('player-one-announcer') && gameTile.classList.contains('empty')) {
+          // Update tile HTML
           gameTile.innerHTML = `${markerOne}`;
           gameTile.classList.remove('empty');
+
+          // Update announcer HTML
           childDiv.innerHTML = 'Player Two\'s turn';
           childDiv.classList.remove('player-one-announcer');
           childDiv.classList.add('player-two-announcer');
+
+          // Update array
+          gameBoard[index] = `${markerOne}`;
+
+          // Check winner
+          checkWinner();
+
         } else if (childDiv.classList.contains('player-two-announcer') && gameTile.classList.contains('empty')) {
+          // Update tile HTML
           gameTile.innerHTML = `${markerTwo}`;
           gameTile.classList.remove('empty');
+
+          // Update announcer HTML
           childDiv.innerHTML = 'Player One\'s turn';
           childDiv.classList.remove('player-two-announcer');
           childDiv.classList.add('player-one-announcer');
+
+          // Update array
+          gameBoard[index] = `${markerTwo}`;
+
+          // Check winner
+          checkWinner();
         }
     });
   };
@@ -67,7 +142,7 @@ const gameModule = (() => {
     const middleFooterDiv = document.querySelector('.middle-footer');
     const announcer = document.createElement('div');
     const text = document.createTextNode('Player One\'s turn');
-    announcer.classList.add('player-one-announcer');
+    announcer.classList.add('announcer','player-one-announcer');
     announcer.appendChild(text);
     middleFooterDiv.appendChild(announcer);
   }
@@ -85,7 +160,9 @@ const gameModule = (() => {
           switchMarkers, 
           populate, 
           clearMiddleFooter, 
-          insertAnnouncer};
+          insertAnnouncer,
+          checkWinner
+        };
 })();
 
 // Player factory
@@ -98,18 +175,11 @@ const createPlayers = () => {
   const markerOne = document.querySelector('.marker-one').innerHTML;
   const markerTwo = document.querySelector('.marker-two').innerHTML;
 
-  const playerOne = playerFactory('player one', markerOne);
-  const playerTwo = playerFactory('player two', markerTwo);
+  const playerOne = playerFactory('Player One', markerOne);
+  const playerTwo = playerFactory('Player Two', markerTwo);
+
+  return (playerOne, playerTwo);
 };
-
-// FUNCTION: announce winner when a player has won
-const announceWinner = () => {
-  const winner = null;
-
-  while (winner = null) {
-    // code here
-  }
-}
 
 // Display tile board
 gameModule.displayBoard();
