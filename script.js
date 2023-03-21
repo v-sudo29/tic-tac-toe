c47f35d2de205ff0eb80862b60b6df941c36bdb9
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-03-20 17:00:57 -0700

    Transfer displayBoard to gameModule

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Global module
 const gameModule = (() => {
+  
+  // Array holds markers
   const gameBoard = ["X", "O", "O", "X", "X", "O", "X", "X", "X"];
-  return {gameBoard};
+
+  // Display game board tiles
+  const displayBoard = () => {
+    const boardContainer = document.querySelector('.gameboard-container');
+    for (let i = 0; i < 9; i++) {
+      const tile = document.createElement('div');
+      tile.setAttribute('id', `${i}`);
+      tile.setAttribute('class', "tile");
+      boardContainer.appendChild(tile);
+    }
+  };
+  return {gameBoard, displayBoard};
 })();
 
 // Players
@@ -9,15 +22,5 @@ const playerFactory = (name, marker) => {
   // code goes here
 };
 
-// FUNCTION: display game board
-function displayBoard() {
-  const boardContainer = document.querySelector('.gameboard-container');
-  for (let i = 0; i < 9; i++) {
-    const tile = document.createElement('div');
-    tile.setAttribute('id', `${i}`);
-    tile.setAttribute('class', "tile");
-    boardContainer.appendChild(tile);
-  }
-}
 
-displayBoard();
+gameModule.displayBoard();
